d5755aa72c6455297f25e532d57085457bdf836a
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2025-10-15 09:27:13 +0300

    review task_2 + started lab_4

--- Lab_2/ex_4.py
+++ Lab_2/ex_4.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
-def predict_next_two_words(prompt_text, model_name="gpt2"):
+def predict_next_word(prompt_text, model_name="gpt2", number_of_words=2):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
     model.eval()
@@ -10,36 +10,39 @@ def predict_next_two_words(prompt_text, model_name="gpt2"):
         tokenizer.pad_token = tokenizer.eos_token
         model.config.pad_token_id = model.config.eos_token_id
 
-    inputs = tokenizer(prompt_text, return_tensors="pt", padding=True)
-    input_ids = inputs["input_ids"]
-    attention_mask = inputs["attention_mask"]
+    for _ in range(number_of_words):
+        inputs = tokenizer(prompt_text, return_tensors="pt", padding=True)
+        input_ids = inputs["input_ids"]
+        attention_mask = inputs["attention_mask"]
 
 
-    output_ids = model.generate(
-        input_ids=input_ids,
-        attention_mask=attention_mask,
-        max_new_tokens=3,
-        num_return_sequences=1,
-        do_sample=False,             
-        eos_token_id=tokenizer.eos_token_id,
-        pad_token_id=tokenizer.pad_token_id
-    )
+        output_ids = model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            max_new_tokens=1,
+            num_return_sequences=1,
+            do_sample=False,             
+            eos_token_id=tokenizer.eos_token_id,
+            pad_token_id=tokenizer.pad_token_id
+        )
 
-    generated_ids = output_ids[0][input_ids.shape[-1]:]
-    generated_text = tokenizer.decode(generated_ids, skip_special_tokens=True)
-    return generated_text.strip()
+        generated_ids = output_ids[0][input_ids.shape[-1]:]
+        generated_text = tokenizer.decode(generated_ids, skip_special_tokens=True)
+        prompt_text += " " + generated_text
+    
+    return prompt_text
 
 
 
 prompts = [
-    "The capital of Romania",
-    "Thanks. I would like",
+    "I want to eat",
     "Artificial intelligence will be",
     "Python is a great",
-    "I am going to"
+    "I am going to",
+    "Thanks. I would like"
 ]
 
 
 for prompt in prompts:
-    continuation = predict_next_two_words(prompt)
-    print(f"\n{prompt}    →   {continuation}")
+    continuare = predict_next_word(prompt, number_of_words=2)
+    print(f"\nPrompt: {prompt} \t|\t New sentence: {continuare}")
